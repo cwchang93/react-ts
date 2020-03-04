6987607fa0208efd981704b724be560512cc83b7
Jinwei Chang <cwchang93@gmail.com>
2020-03-04 08:46:45 +0800

    Update file folder

--- src/components/04-voteSample/Group.tsx
+++ src/components/04-voteSample/Group.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import * as React from "react";
+
 
 interface I_Props {
   name: string;
--- src/components/04-voteSample/GroupList.tsx
+++ src/components/04-voteSample/GroupList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import Group from "./Group";
 
 interface I_f2eGroup {
